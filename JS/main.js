75d52139e3759d1c6030a5883fa61e25e27d5f4f
sandy <sandra.de.la.sancha.aguilar.isc.2000@gmail.com>
2025-04-09 16:29:30 -0600

    Se agrega la funcionalidad en el main.js

--- JS/main.js
+++ JS/main.js
@@ -1,22 +1,26 @@
-let encabezado1 = document.getElementsByTagName("h1").item(0);
+//////////////////VARIABLES///////////////////////////////
+let txtNombre = document.getElementById("txtNombre");
+let btnNombre = document.getElementById ("btnNombre");
+
+btnNombre.addEventListener("click", function(event){
+    event.preventDefault();
+    let nombre=txtNombre.value;
+    localStorage.setItem("nombre", nombre);
+});
 
 
-if (localStorage.getItem("email") != null) {
-    let email = localStorage.getItem("email");
-    encabezado1.innerText = `Bienvenido de nuevo ${email}`;
-    console.log(email);
+//if (localStorage.getItem("email") != null) {
+  //  let email = localStorage.getItem("email");
+  //  encabezado1.innerText = `Bienvenido de nuevo ${email}`;
+  //  console.log(email);
 
 
-    localStorage.removeItem("email");//Elimina una variable
-    localStorage.clear(); //Limpia los conjuntos de clave/valor
+  //  localStorage.removeItem("email");//Elimina una variable
+   // localStorage.clear(); //Limpia los conjuntos de clave/valor
 
 
-} else {
+//} else {
     //guarda el correo electronico en el navegador
-    localStorage.setItem("email", "jperez@hotmail.com");
+ //   localStorage.setItem("email", "jperez@hotmail.com");
 
-}//!null
-
-
-//Guarda el correo electronico en el navegador
-//localStorage.setItem("email", "jperez@hotmail.com");
+//}//!null
